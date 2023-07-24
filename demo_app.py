d42db74ff97138a029770e99f6f313f18508493b
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-24 17:43:53 +0200

    Update demo_app.py

--- demo_app.py
+++ demo_app.py
@@ -4,7 +4,7 @@ from src.supabase_connector import SupabaseConnection
 
 def main():
     # Display the logo at the top
-    st.sidebar.image("supabase-logo.png", use_column_width=False)
+    st.sidebar.image("supabase-logo.png", use_column_width=False) st.sidebar.image("streamlit-logo.png", use_column_width=False)
 
     # Sidebar content
     st.sidebar.header("Supabase + Streamlit")
