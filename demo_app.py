7a07f0606015c4f2d890206c858b55405ca06aee
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-24 17:02:34 +0200

    updates

--- demo_app.py
+++ demo_app.py
@@ -1,47 +1,36 @@
-# Streamlit for web app interface
 import streamlit as st
-
-# Polars for data manipulation and display
 import polars as pl
-
-# Custom Supabase connection class
 from src.supabase_connector import SupabaseConnection
 
 def main():
-    """
-    Main function to run the Streamlit app. It provides an interface to select a table,
-    choose specific columns, and fetch data from the selected table in Supabase.
-    """
-    
-    # Set the title of the app
     st.title("Streamlit App with Supabase")
 
-    # Initialize the custom Supabase connection
+    # Initialize our custom Supabase connection
     conn = st.experimental_connection("supabase_conn", type=SupabaseConnection)
 
-    # Dropdown menu to select a table from the available options
+    # Dropdown to select a table
     tables = ["crypto_analysis", "customers"]
     selected_table = st.selectbox("Select a table:", tables)
 
-    # Determine available columns based on the selected table
-    if selected_table == "crypto_analysis":
-        available_columns = ["column1", "column2", "column3"]  # Replace with actual column names
-    elif selected_table == "customers":
-        available_columns = ["first_name", "last_name"]
+    # If the 'customers' table is selected, show UI for inserting data
+    if selected_table == "customers":
+        st.subheader("Insert Data into Customers Table")
+        first_name = st.text_input("First Name")
+        last_name = st.text_input("Last Name")
 
-    # Multi-select widget for users to choose specific columns
-    selected_columns = st.multiselect("Select columns:", available_columns, default=available_columns)
+        if st.button("Insert Data"):
+            if first_name and last_name:
+                inserted_data = conn.insert_data("customers", {"first_name": first_name, "last_name": last_name})
+                st.success("Data inserted successfully!")
+            else:
+                st.warning("Please fill in both fields before inserting.")
 
-    # Convert the list of selected columns into a comma-separated string
-    columns_str = ",".join(selected_columns)
-
-    # Button to fetch and display data from the selected table and columns
+    # Button to fetch data (available for all tables)
     if st.button("Fetch Data from Selected Table"):
-        data = conn.fetch_data(selected_table, selected_columns=columns_str)
+        data = conn.fetch_data(selected_table)
         if data:
             df = pl.DataFrame(data)
             st.write(df)
 
-# Run the main function when the script is executed
 if __name__ == "__main__":
     main()
--- src/supabase_connector.py
+++ src/supabase_connector.py
@@ -2,17 +2,15 @@ from streamlit.connections import ExperimentalBaseConnection
 from supabase import create_client
 
 class SupabaseConnection(ExperimentalBaseConnection):
-    def __init__(self, **kwargs):
-        # Initialize Supabase client using secrets
+    def _connect(self, **kwargs):
         url = st.secrets["SUPABASE"]["URL"]
         api_key = st.secrets["SUPABASE"]["API_KEY"]
-        self._instance = create_client(url, api_key)
+        return create_client(url, api_key)
 
-    def fetch_data(self, table_name, selected_columns="*"):
-        query = self._instance.table(table_name).select(selected_columns)
-        response = query.execute()
+    def fetch_data(self, table_name):
+        response = self._instance.table(table_name).select("*").execute()
         return response.data
 
-    def insert_data(self, table_name, data):
-        response = self._instance.table(table_name).insert(data).execute()
+    def insert_data(self, table_name, values):
+        response = self._instance.table(table_name).insert(values).execute()
         return response.data
