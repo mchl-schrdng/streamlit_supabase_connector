b0243c405eff386327afca80d33121531b0bc71f
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-24 17:12:44 +0200

    updates

--- demo_app.py
+++ demo_app.py
@@ -3,12 +3,17 @@ import polars as pl
 from src.supabase_connector import SupabaseConnection
 
 def main():
-    st.title("Streamlit App with Supabase")
+    # Display the logo at the top
+    st.image("supabase-logo.png", use_column_width=True)
+
+    # Sidebar content
+    st.sidebar.header("Supabase + Streamlit")
+    st.sidebar.text("This app demonstrates the use of Streamlit's ExperimentalBaseConnection to create a connection with Supabase.")
 
     # Initialize our custom Supabase connection
     conn = st.experimental_connection("supabase_conn", type=SupabaseConnection)
 
-    # Dropdown to select a table
+    # Dropdown to select a table in the main area
     tables = ["crypto_analysis", "customers"]
     selected_table = st.selectbox("Select a table:", tables)
 
@@ -20,7 +25,6 @@ def main():
 
         if st.button("Insert Data"):
             if first_name and last_name:
-                # Insert data into the 'customers' table
                 inserted_data = conn.insert_data("customers", {"first_name": first_name, "last_name": last_name})
                 st.success("Data inserted successfully!")
             else:
