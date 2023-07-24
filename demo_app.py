705a8b3dc5f749e9a8c5932251795b1de6df4b53
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-24 17:00:07 +0200

    updates

--- demo_app.py
+++ demo_app.py
@@ -1,33 +1,47 @@
+# Streamlit for web app interface
 import streamlit as st
+
+# Polars for data manipulation and display
 import polars as pl
+
+# Custom Supabase connection class
 from src.supabase_connector import SupabaseConnection
 
 def main():
+    """
+    Main function to run the Streamlit app. It provides an interface to select a table,
+    choose specific columns, and fetch data from the selected table in Supabase.
+    """
+    
+    # Set the title of the app
     st.title("Streamlit App with Supabase")
 
-    # Initialize our custom Supabase connection
+    # Initialize the custom Supabase connection
     conn = st.experimental_connection("supabase_conn", type=SupabaseConnection)
 
-    # Dropdown to select a table
+    # Dropdown menu to select a table from the available options
     tables = ["crypto_analysis", "customers"]
     selected_table = st.selectbox("Select a table:", tables)
 
-    # Multi-select for columns
+    # Determine available columns based on the selected table
     if selected_table == "crypto_analysis":
         available_columns = ["column1", "column2", "column3"]  # Replace with actual column names
     elif selected_table == "customers":
         available_columns = ["first_name", "last_name"]
+
+    # Multi-select widget for users to choose specific columns
     selected_columns = st.multiselect("Select columns:", available_columns, default=available_columns)
 
-    # Convert selected columns to a comma-separated string
+    # Convert the list of selected columns into a comma-separated string
     columns_str = ",".join(selected_columns)
 
-    # Fetch Data Button
+    # Button to fetch and display data from the selected table and columns
     if st.button("Fetch Data from Selected Table"):
         data = conn.fetch_data(selected_table, selected_columns=columns_str)
         if data:
             df = pl.DataFrame(data)
             st.write(df)
 
+# Run the main function when the script is executed
 if __name__ == "__main__":
     main()
